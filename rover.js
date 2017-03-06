fc39e35c0117f63e3cd70fcd792cdf68bc3cf549
Isak La Fleur <isak.engdahl@gmail.com>
2017-03-06 21:24:13 +0100

    final commit.

--- rover.js
+++ rover.js
@@ -1,7 +1,7 @@
-gridsize = [10,10];
-direction = 'fffrfflfffbb' // direction string
+gridsize = [20,20]; // size of grid the rover can travel in
+direction = 'fffrfflfffbbfffffffffffffffffflrlrffffff' // direction string
 directionArray = direction.split('');
-//console.log(directionArray);
+// console.log(directionArray);
 position = [0,0]; // [x,y]
 x = position[0];
 y = position[1];
@@ -14,18 +14,21 @@ function rover(x,y){
 }
 
 rover.move = function() {
-    for (i = 0; i < (directionArray.length + 1); i++) {
+    for (i = 0; i <= directionArray.length; i++) {
         var dir = direction[i];
-        
+
+        // wraps from one edge of the grid to another
+        // ex. with a gridsize of 10x10 => (0,1,2,3,4,5,6,7,8,9,10,0,1,2,.,.)
+
         if (face === 'N'){
             switch(dir) {
                 case 'r': face = 'E';
                 break;
                 case 'l': face = 'W';
                 break;
-                case 'f': y += 1;
+                case 'f': y === gridsize[1] ? y = 0 : y += 1;
                 break;
-                case 'b': y -= 1;
+                case 'b': y === 0 ? y = gridsize[1] : y -= 1;
                 break;
             }
         }
@@ -35,9 +38,9 @@ rover.move = function() {
                 break;
                 case 'l': face = 'N';
                 break;
-                case 'f': x += 1;
+                case 'f': x === gridsize[0] ? x = 0 : x += 1;
                 break;
-                case 'b': x -= 1;
+                case 'b': x === 0 ? x = gridsize[0] : x -= 1;
                 break;
             }
         }
@@ -47,9 +50,9 @@ rover.move = function() {
                 break;
                 case 'l': face = 'S';
                 break;
-                case 'f': x -= 1;
+                case 'f': x === 0 ? x = gridsize[0] : x -= 1;
                 break;
-                case 'b': x += 1;
+                case 'b': x === gridsize[0] ? x = 0 : x += 1;
                 break;
             }
         }
@@ -59,35 +62,14 @@ rover.move = function() {
                 break;
                 case 'l': face = 'E';
                 break;
-                case 'f': y -= 1;
+                case 'f': y === 0 ? y = gridsize[1] : y -= 1;
                 break;
-                case 'b': y += 1;
+                case 'b': y === gridsize[1] ? y = 0 : y += 1;
                 break;
             }
         }
-        else {
-            console.log('Command not valid! Looks like your are tired, go back to earth!');
-        }
     }
-};
-
-var grid = {
-    x: gridsize[0],
-    y: gridsize[1]
-};
-
-rover.checkGrid = function() {
-    if (gridsize[1] < Math.abs(y)){
-        y = (Math.abs(y) % (gridsize[1] / 2));
-    }
-    if (gridsize[0] < Math.abs(x)){
-        x = (Math.abs(x) % (gridsize[0]/2));
-    }
-    else {
-        console.log('Your end position is [' + [x,y] + ']');
-        return true;
-    }console.log('Your end position is [' + [x,y] + ']');
 };
 
 rover.move(position[0], position[1]);
-rover.checkGrid();
+console.log('Your end position is [' + [x,y] + ']');
